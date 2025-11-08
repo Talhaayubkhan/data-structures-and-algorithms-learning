d26e98f7ec1013447def7e6bb40152b3eebd7b4d
Talha Ayub <103215272+Talhaayubkhan@users.noreply.github.com>
2025-11-08 22:14:02 +0500

    Fix binary search loop condition for clarity

--- Searching & Sorting/BinarySearch.js
+++ Searching & Sorting/BinarySearch.js
@@ -2,7 +2,7 @@ function binarySearch(arr, target) {
   let left = 0;
   let right = arr.length - 1;
 
-  while (left <= right) {
+  while (right >= left) {
     // calculate the middle
     let middle = Math.floor((left + right) / 2);
 
